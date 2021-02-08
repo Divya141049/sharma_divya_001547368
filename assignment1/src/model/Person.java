8fd73083fcd70ab91d8e2db52e8fc67e5392dd42
Divya141049 <sharma.divya1@northeastern.edu>
2021-02-07 22:52:57 -0500

    assignment1 submission

---
+++ assignment1/src/model/Person.java
@@ -0,0 +1,321 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Divya Sharma
+ */
+public class Person {
+    private String LastName;
+    private String FirstName;
+    private String Phone;
+    private String DOB;
+    private String Age;
+    private String Street1;
+    private String Street2;
+    private String City;
+    private String State;
+    private String Zip;
+    private String SBankname;
+    private String SRoutingno;
+    private String SAccno;
+    private String SAccbal;
+    private String SAcctype;
+    private String CBankname;
+    private String CRoutingno;
+    private String CAccno;
+    private String CAccbal;
+    private String CAcctype;
+    private String Licenseno;
+    private String Issuedate;
+    private String Expirydate;
+    private String Bloodgroup;
+    private String Image;
+    private String Medicalrecordno;
+    private String Allergy1;
+    private String Allergy2;
+    private String Allergy3;
+
+    public String getMedicalrecordno() {
+        return Medicalrecordno;
+    }
+
+    public void setMedicalrecordno(String Medicalrecordno) {
+        this.Medicalrecordno = Medicalrecordno;
+    }
+
+    public String getAllergy1() {
+        return Allergy1;
+    }
+
+    public void setAllergy1(String Allergy1) {
+        this.Allergy1 = Allergy1;
+    }
+
+    public String getAllergy2() {
+        return Allergy2;
+    }
+
+    public void setAllergy2(String Allergy2) {
+        this.Allergy2 = Allergy2;
+    }
+
+    public String getAllergy3() {
+        return Allergy3;
+    }
+
+    public void setAllergy3(String Allergy3) {
+        this.Allergy3 = Allergy3;
+    }
+    
+    
+    
+    
+
+    public String getLicenseno() {
+        return Licenseno;
+    }
+
+    public void setLicenseno(String Licenseno) {
+        this.Licenseno = Licenseno;
+    }
+
+    public String getIssuedate() {
+        return Issuedate;
+    }
+
+    public void setIssuedate(String Issuedate) {
+        this.Issuedate = Issuedate;
+    }
+
+    public String getExpirydate() {
+        return Expirydate;
+    }
+
+    public void setExpirydate(String Expirydate) {
+        this.Expirydate = Expirydate;
+    }
+
+    public String getBloodgroup() {
+        return Bloodgroup;
+    }
+    
+    public String getImagePath() {
+        return Image;
+    }
+
+    public void setBloodgroup(String Bloodgroup) {
+        this.Bloodgroup = Bloodgroup;
+    }
+    
+    public void setImage(String Image) {
+        this.Image = Image;
+    }
+    
+
+    public String getCBankname() {
+        return CBankname;
+    }
+
+    public void setCBankname(String CBankname) {
+        this.CBankname = CBankname;
+    }
+
+    public String getCRoutingno() {
+        return CRoutingno;
+    }
+
+    public void setCRoutingno(String CRoutingno) {
+        this.CRoutingno = CRoutingno;
+    }
+
+    public String getCAccno() {
+        return CAccno;
+    }
+
+    public void setCAccno(String CAccno) {
+        this.CAccno = CAccno;
+    }
+
+    public String getCAccbal() {
+        return CAccbal;
+    }
+
+    public void setCAccbal(String CAccbal) {
+        this.CAccbal = CAccbal;
+    }
+
+    public String getCAcctype() {
+        return CAcctype;
+    }
+
+    public void setCAcctype(String CAcctype) {
+        this.CAcctype = CAcctype;
+    }
+    
+    
+
+    public String getSBankname() {
+        return SBankname;
+    }
+
+    public void setSBankname(String SBankname) {
+        this.SBankname = SBankname;
+    }
+
+    public String getSRoutingno() {
+        return SRoutingno;
+    }
+
+    public void setSRoutingno(String SRoutingno) {
+        this.SRoutingno = SRoutingno;
+    }
+
+    public String getSAccno() {
+        return SAccno;
+    }
+
+    public void setSAccno(String SAccno) {
+        this.SAccno = SAccno;
+    }
+
+    public String getSAccbal() {
+        return SAccbal;
+    }
+
+    public void setSAccbal(String SAccbal) {
+        this.SAccbal = SAccbal;
+    }
+
+    public String getSAcctype() {
+        return SAcctype;
+    }
+
+    public void setSAcctype(String SAcctype) {
+        this.SAcctype = SAcctype;
+    }
+    
+    
+    
+    
+    
+
+    public Person() {
+    }
+    
+
+    public String getStreet1() {
+        return Street1;
+    }
+
+    public void setStreet1(String Street1) {
+        this.Street1 = Street1;
+    }
+
+    public String getStreet2() {
+        return Street2;
+    }
+
+    public void setStreet2(String Street2) {
+        this.Street2 = Street2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZip() {
+        return Zip;
+    }
+
+    public void setZip(String Zip) {
+        this.Zip = Zip;
+    }
+    
+    
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+    private String height;
+    private String weight;
+    private String SSN;
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+    
+    
+}
